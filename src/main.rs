118ae7336acfdf2261afde6c791cf3075bb11aa9
Aren Blondahl <rean1985@gmail.com>
2021-12-11 21:14:13 -0700

    Changed challenge to take input file and fixed err flow

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,41 @@ extern crate clap;
 use clap::{App, Arg, ArgMatches, SubCommand};
 
 trait Challenge {
-    fn run(&self);
+    fn run(&self) -> Result<(), String>;
 }
 
 struct DayOneChallenge {
-
-}
-
-impl DayOneChallenge {
-    fn from<'a>(args: &'a ArgMatches) -> Self {
-        let values = args.values_of("values");
-
-        for v in values {
-            println!("Found Value: {:?}", v);
-        }
-
-        println!("Initialized");
-
-        Self {
-
-        }
-    }
+    input_file: String
 }
 
 impl Challenge for DayOneChallenge {
-    fn run(&self) {
-        println!("Ran");
+    fn run(&self) -> Result<(), String> {
+        println!("Loading file: {}", self.input_file);
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+enum ArgumentError {
+    InvalidSubcommand,
+    MissingInput,
+}
+
+impl<'a> TryFrom<&ArgMatches<'a>> for DayOneChallenge {
+    type Error = ArgumentError;
+
+    fn try_from(matches: &ArgMatches<'a>) -> Result<DayOneChallenge, Self::Error> {
+        let mut obj = DayOneChallenge {
+            input_file: "".to_string()
+        };
+        
+        if let Some(input_file) = matches.value_of("INPUT") {
+            obj.input_file = input_file.to_string();
+        } else {
+            return Err(ArgumentError::MissingInput);
+        }
+
+        return Ok(obj);
     }
 }
 
@@ -36,14 +45,25 @@ fn main() {
     let matches = App::new("aocode21")
         .version("2021.0.0")
         .subcommand(SubCommand::with_name("ch01")
-            .arg(Arg::with_name("values")
-                .multiple(true)))
+            .arg(Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1)))
         .get_matches();
 
-    match matches.subcommand() {
-        ("ch01", Some(sub_m)) => {
-            DayOneChallenge::from(sub_m).run()
-        },
-        _ => {}
+    let challenge = match matches.subcommand() {
+        ("ch01", Some(sub_m)) => DayOneChallenge::try_from(sub_m),
+        _ => Err(ArgumentError::InvalidSubcommand)
+    };
+
+    let result = match challenge {
+        Ok(challenge) => challenge.run(),
+        Err(ArgumentError::InvalidSubcommand) => Err(format!("Unknown subcommand: {}", matches.subcommand_name().unwrap())),
+        Err(ArgumentError::MissingInput) => Err(format!("Missing input file.")),
+    };
+
+    match result {
+        Ok(_) => {},
+        Err(err) => eprintln!("ERR: {}", err)
     }
 }
