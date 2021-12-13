9187bf4b011d2b0ccecb90f4994c5bf4a8a492bd
Aren Blondahl <rean1985@gmail.com>
2021-12-12 23:51:13 -0700

    Day Two Part Two implemented

--- src/challenge02.rs
+++ src/challenge02.rs
@@ -5,7 +5,8 @@ use crate::common::{banner, ChallengeData};
 
 struct Submarine {
     position: i32,
-    depth: i32
+    depth: i32,
+    aim: i32
 }
 
 impl Submarine {
@@ -14,13 +15,26 @@ impl Submarine {
         self.depth = self.depth - by;
     }
 
+    fn up_aim(&mut self, by: i32) {
+        self.aim = self.aim - by;
+    }
+
     /// Note the "depth" is decreased by going up
     fn down(&mut self, by: i32) {
         self.depth = self.depth + by;
     }
 
+    fn down_aim(&mut self, by: i32) {
+        self.aim = self.aim + by;
+    }
+
     fn forward(&mut self, by: i32) {
         self.position = self.position + by;
+    }
+
+    fn forward_with_aim(&mut self, by: i32) {
+        self.position = self.position + by;
+        self.depth = self.depth + (self.aim * by);
     }
 
     fn compute(&self) -> i32 {
@@ -30,7 +44,7 @@ impl Submarine {
 
 impl Default for Submarine {
     fn default() -> Self {
-        Self { position: 0, depth: 0 }
+        Self { position: 0, depth: 0, aim: 0 }
     }
 }
 
@@ -65,17 +79,41 @@ pub fn day_two_challenge(data: &ChallengeData) -> Result<()> {
         bail!("Some inputs are not valid directions:\n{}", failures);
     }
 
+    banner("02 - Part 1", &data.input_file);
+    part_one(&lines)?;
+    banner("02 - Part 2", &data.input_file);
+    part_two(&lines)?;
+
+    Ok(())
+}
+
+fn part_one(lines: &Vec<Direction>) -> Result<()> {
     let mut submarine = Submarine::default();
 
     for line in lines {
-        match line {
+        match *line {
             Direction::Forward(x) => { submarine.forward(x) },
             Direction::Up(x) => { submarine.up(x) },
             Direction::Down(x) => { submarine.down(x) },
         }
     }
 
-    banner("02 - Part 1", &data.input_file);
+    let submarine = submarine;
+    println!("Position: {}, Depth: {}, Result: {}", submarine.position, submarine.depth, submarine.compute());
+
+    Ok(())
+}
+
+fn part_two(lines: &Vec<Direction>) -> Result<()> {
+    let mut submarine = Submarine::default();
+
+    for line in lines {
+        match *line {
+            Direction::Forward(x) => { submarine.forward_with_aim(x) },
+            Direction::Up(x) => { submarine.up_aim(x) },
+            Direction::Down(x) => { submarine.down_aim(x) },
+        }
+    }
 
     let submarine = submarine;
     println!("Position: {}, Depth: {}, Result: {}", submarine.position, submarine.depth, submarine.compute());
